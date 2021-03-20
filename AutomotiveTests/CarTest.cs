32a439b57ddce511f148d435ac4ad9460842825d
Przemysław Barć <w63625@student.wsiz.rzeszow.pl>
2021-03-20 18:48:24 +0100

    Initial commit

---
+++ AutomotiveTests/CarTest.cs
@@ -0,0 +1,17 @@
+﻿using Automotive;
+using System;
+using Xunit;
+using FluentAssertions;
+namespace AutomotiveTests
+{
+    public class CarTest
+    {
+        [Fact]
+        public void Can_create_valid_car()
+        {
+            var car = new Car("Red", "Ford Focus",5.5, 60);
+
+            car.Should().NotBeNull();
+        }
+    }
+}
